b64308b688d20f05d4465836ca6011b8c4872b49
Andrés F. Alvarez <andresfelipe.alvarez03@gmail.com>
2023-04-12 18:28:34 -0500

    Ajustar los estilos basicos del sitio

--- encriptadorTexto/script.js
+++ encriptadorTexto/script.js
@@ -1,1 +1,20 @@
-document.write('hola mundo desde archivo script')
+const criptos = {
+  'a':'un valor para a',
+  'e':'un valor para e',
+  'i':'un valor para i',
+  'o':'un valor para o',
+  'u':'un valor para u',
+}
+
+console.log('hola mundo desde archivo script')
+console.log('<\BR>')
+console.log(criptos.a)
+console.log('<\BR>')
+console.log(criptos.e)
+console.log('<\BR>')
+console.log(criptos.i)
+console.log('<\BR>')
+console.log(criptos.o)
+console.log('<\BR>')
+console.log(criptos.u)
+
